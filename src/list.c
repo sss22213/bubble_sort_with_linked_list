2c80d6ac372be2f572c332488592a1593ce945f3
sss22213 <n0404.n0404@gmail.com>
2020-10-21 03:12:09 -0700

    fix

--- src/list.c
+++ src/list.c
@@ -46,46 +46,34 @@ void remove_node(LIST* list, NODE* target)
 
 void swap_node(LIST* list, NODE* node1, NODE* node2)
 {
-	// null
+	// NULL
 	if( node1 == node2 || node1 == NULL || node2 == NULL)
 		return;
 	// avoid loop
-	if( node2->next == node1 )
+	if( node2->next == node1 || node2 == list->head)
 	{
 		NODE *tmp = node1;
 		node1 = node2;
 		node2 = tmp;
 	}
-	// fake head
-	NODE *fake_head = new_node(-1);
-	fake_head->next = list->head;
-	// ptr target
-	NODE **ptr_find_target = &fake_head;
-	NODE **ptr_target_pre_1 = NULL;
-	NODE **ptr_target_pre_2 = NULL;
-	while (*ptr_find_target != NULL)
+	NODE **ptr_target1 = &list->head;
+	while( *ptr_target1 != node1)
 	{
-		if((*ptr_find_target)->next == node1)
-			ptr_target_pre_1 = ptr_find_target;
-		if((*ptr_find_target)->next == node2)
-			ptr_target_pre_2 = ptr_find_target;
-		ptr_find_target = &(*ptr_find_target)->next;
+		if(*ptr_target1 == NULL)return;
+		ptr_target1 = &(*ptr_target1)->next;
 	}
-	// not found
-	if(ptr_target_pre_1 == NULL || ptr_target_pre_2 == NULL)
-		return;
+	NODE **ptr_target2 = &node1;
+	while( *ptr_target2 != node2)
+	{
+		if(*ptr_target2 == NULL)return;
+		ptr_target2 = &(*ptr_target2)->next;
+	}
 	// swap
-	NODE *tmp_node_1_nxt = node1->next;
-	NODE *tmp_node_2_nxt = node2->next;
-	(*ptr_target_pre_1)->next = node2;
-	node2->next = tmp_node_1_nxt;
-	(*ptr_target_pre_2)->next = node1;
-	node1->next = tmp_node_2_nxt;
-	// head
-	if(node1 == list->head)
-		list->head = node2;
-	else if(node2 == list->head)
-		list->head = node1;
+	NODE *tmp = node2->next;
+	*ptr_target1 = node2;
+	*ptr_target2 = node1;
+	node2->next = node1->next;
+	node1->next = tmp;
 }
 
 void list_display(LIST *list)
--- src/main.c
+++ src/main.c
@@ -14,7 +14,9 @@ int main()
 	insert_node(new_list_list, node_2, node_3);
 	insert_node(new_list_list, node_3, node_4);
 	list_display(new_list_list);
-	swap_node(new_list_list, node_4, node_head);
+	swap_node(new_list_list, node_head, node_1);
+	list_display(new_list_list);
+	bubble_sort(new_list_list);
 	list_display(new_list_list);
 	return 0;
 }
